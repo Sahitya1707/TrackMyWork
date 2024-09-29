d91f8842d58cde7b484b46e77639dcb2ead8ecd8
Sahitya hp <neupanesahitya1@gmail.com>
2024-09-29 10:31:49 -0400

    Add project files.

---
+++ TrackMyWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrackMyWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
