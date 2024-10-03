e65d438dd4b0d3b22d546ce6940864e6650f2c2f
Sahitya hp <neupanesahitya1@gmail.com>
2024-10-02 23:35:16 -0400

    added connection string in application.json and run migration

--- TrackMyWork/Data/Migrations/20241003033158_InitialCreate_v2_track_my_work.Designer.cs
+++ TrackMyWork/Data/Migrations/20241003033158_InitialCreate_v2_track_my_work.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrackMyWork.Data;
 
@@ -11,9 +12,11 @@ using TrackMyWork.Data;
 namespace TrackMyWork.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241003033158_InitialCreate_v2_track_my_work")]
+    partial class InitialCreate_v2_track_my_work
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TrackMyWork/Data/Migrations/20241003033158_InitialCreate_v2_track_my_work.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackMyWork.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_v2_track_my_work : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
