babce3b0ae9876cafd610a9a0820a72800df0271
Sahitya hp <neupanesahitya1@gmail.com>
2024-10-10 12:00:36 -0400

    toggle button with overlay added

--- TrackMyWork/wwwroot/js/site.js
+++ TrackMyWork/wwwroot/js/site.js
@@ -5,11 +5,23 @@
 const hamBurger = document.getElementById('hamBurger');
 const navMenu = document.getElementById('navMenu');
 const closeNav = document.getElementById('closeNav');
+const overlay = document.getElementById('overlay')
+const body = document.querySelector('body')
+
 
 hamBurger.addEventListener('click', () => {
     navMenu.style.transform = 'translateX(0)';
-});
+    body.classList.add('bodyFixed');
+    overlay.classList.remove('hidden')
 
-closeNav.addEventListener('click', () => {
-    navMenu.style.transform = 'translateX(100%)';
 });
+// defining close nav function
+const closeMenu = () => {
+    console.log('closenav called')
+    navMenu.style.transform = 'translateX(100%)';
+    body.classList.remove('bodyFixed');
+    overlay.classList.add('hidden')
+}
+
+closeNav.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu)
