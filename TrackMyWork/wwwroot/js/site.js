41bfe13da59193b19ece6fbd15efc94eeff8534b
Sahitya hp <neupanesahitya1@gmail.com>
2024-10-10 02:45:38 -0400

    navigation has been made

--- TrackMyWork/wwwroot/js/site.js
+++ TrackMyWork/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const hamBurger = document.getElementById('hamBurger');
+const navMenu = document.getElementById('navMenu');
+const closeNav = document.getElementById('closeNav');
+
+hamBurger.addEventListener('click', () => {
+    navMenu.style.transform = 'translateX(0)';
+});
+
+closeNav.addEventListener('click', () => {
+    navMenu.style.transform = 'translateX(100%)';
+});
